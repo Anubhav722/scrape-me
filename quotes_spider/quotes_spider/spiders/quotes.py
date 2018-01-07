1d543d0f6aee1af520e96826dd506d580e2b2303
Anubhav722 <anubhavs286@gmail.com>
2018-01-07 16:08:35 +0530

    update spider to use items.py and pipelines.py file

--- quotes_spider/quotes_spider/spiders/quotes.py
+++ quotes_spider/quotes_spider/spiders/quotes.py
@@ -1,40 +1,50 @@
 # -*- coding: utf-8 -*-
-import scrapy
+from scrapy import Spider
+from scrapy.loader import ItemLoader
+
+from quotes_spider.items import QuotesSpiderItem
 
 
-class QuotesSpider(scrapy.Spider):
+class QuotesSpider(Spider):
     name = 'quotes'
     allowed_domains = ['quotes.toscrape.com']
     start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
 
+
+    	l = ItemLoader(item=QuotesSpiderItem(), response=response)
     	# Commenting this out. Just for understanding.
-        # h1_tag = response.xpath('//h1/a/text()').extract_first()
-        # tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
+        h1_tag = response.xpath('//h1/a/text()').extract_first()
+        tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
 
         # yield {'H1 Tag': h1_tag, 'Tags': tags}
+        l.add_value('h1_tag', h1_tag)
+        l.add_value('tags', tags)
 
-        quotes = response.xpath('//*[@class="quote"]')
-        for quote in quotes:
-        	text = quote.xpath('.//*[@class="text"]/text()').extract_first()
-        	author = quote.xpath('.//*[@itemprop="author"]/text()').extract_first()
-        	tags = quote.xpath('.//*[@itemprop="keywords"]/@content').extract_first()
-        	# tags = quote.xpath('.//*[@class="tag"]/text()').extract()
+        return l.load_item()
 
 
-        	# If you want to print the data.
-        	# print('\n')
-        	# print(text)
-        	# print(author)
-        	# print(tags)
-        	# print('\n')
+        # quotes = response.xpath('//*[@class="quote"]')
+        # for quote in quotes:
+        # 	text = quote.xpath('.//*[@class="text"]/text()').extract_first()
+        # 	author = quote.xpath('.//*[@itemprop="author"]/text()').extract_first()
+        # 	tags = quote.xpath('.//*[@itemprop="keywords"]/@content').extract_first()
+        # 	# tags = quote.xpath('.//*[@class="tag"]/text()').extract()
 
-        	yield{'Text':text,
-        		  'Author':author,
-        		  'Tags':tags}
 
-        next_page_url = response.xpath('//*[@class="next"]/a/@href').extract_first()
-        absolute_next_page_url = response.urljoin(next_page_url)
+        # 	# If you want to print the data.
+        # 	# print('\n')
+        # 	# print(text)
+        # 	# print(author)
+        # 	# print(tags)
+        # 	# print('\n')
 
-        yield scrapy.Request(absolute_next_page_url)
+        # 	yield{'Text':text,
+        # 		  'Author':author,
+        # 		  'Tags':tags}
+
+        # next_page_url = response.xpath('//*[@class="next"]/a/@href').extract_first()
+        # absolute_next_page_url = response.urljoin(next_page_url)
+
+        # yield scrapy.Request(absolute_next_page_url)

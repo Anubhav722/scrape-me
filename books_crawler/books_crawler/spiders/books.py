1e583acecb5a772c1fc6d0ea359982b854fbf3ba
Anubhav722 <anubhavs286@gmail.com>
2018-01-08 14:21:09 +0530

    add scrapy selectors with selenium

--- books_crawler/books_crawler/spiders/books.py
+++ books_crawler/books_crawler/spiders/books.py
@@ -1,18 +1,48 @@
 # -*- coding: utf-8 -*-
-from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor
 
-# Using CrawlSpider here.
-class BooksSpider(CrawlSpider):
+# from scrapy.spiders import CrawlSpider, Rule
+from scrapy import Spider
+from scrapy.linkextractors import LinkExtractor
+from scrapy.selector import Selector
+from scrapy.http import Request
+
+# Selenium imports
+from selenium import webdriver
+
+
+class BooksSpider(Spider):
     name = 'books'
     allowed_domains = ['books.toscrape.com']
-    start_urls = ['http://books.toscrape.com/']
 
-    # passing args
-    # rules = (Rule(LinkExtractor(deny_domains=('google.com')), callback='parse_page', follow=True),)
-    rules = (Rule(LinkExtractor(allow=('music')), callback='parse_page', follow=True),)
+    def start_requests(self):
+    	# import ipdb; ipdb.set_trace()
+    	# we won't get the response since start_urls is not defined.
+    	self.driver = webdriver.Chrome('/home/sheron/Downloads/chromedriver')
+    	self.driver.get('http://books.toscrape.com')
 
-    # rules = (Rule(LinkExtractor(), callback='parse_page', follow=True),)
+    	# Creating a selector
+    	sel = Selector(text=self.driver.page_source)
+    	book_urls = sel.xpath('//h3/a/@href').extract()
 
-    def parse_page(self, response):
-        pass
+    	for book_url in book_urls:
+    		url = 'http://books.toscrape.com/' + book_url
+    		yield Request(url, callback=self.parse_book)
+
+    def parse_book(self, response):
+    	pass
+
+
+# Using CrawlSpider here.
+# class BooksSpider(CrawlSpider):
+#     name = 'books'
+#     allowed_domains = ['books.toscrape.com']
+#     start_urls = ['http://books.toscrape.com/']
+
+#     # passing args
+#     # rules = (Rule(LinkExtractor(deny_domains=('google.com')), callback='parse_page', follow=True),)
+#     rules = (Rule(LinkExtractor(allow=('music')), callback='parse_page', follow=True),)
+
+#     # rules = (Rule(LinkExtractor(), callback='parse_page', follow=True),)
+
+#     def parse_page(self, response):
+#         pass

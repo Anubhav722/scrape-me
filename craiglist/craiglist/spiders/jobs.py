53e3d9f6161286526a77cb0cd8db0e7cd6be4e14
Anubhav722 <anubhavs286@gmail.com>
2018-01-11 02:49:14 +0530

    add spider to fetch job title listing

--- craiglist/craiglist/spiders/jobs.py
+++ craiglist/craiglist/spiders/jobs.py
@@ -8,4 +8,8 @@ class JobsSpider(scrapy.Spider):
     start_urls = ['https://newyork.craigslist.org/search/egr']
 
     def parse(self, response):
-        pass
+        listings = response.xpath('//a[@class="result-title hdrlnk"]/text()').extract()
+
+        for listing in listings:
+        	# print(listing)
+        	yield {'Listing': listing}

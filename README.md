3b5d52b17d686ff8c2f69752e3e9cdbc313468cb
Anubhav722 <anubhavs286@gmail.com>
2018-01-11 00:42:44 +0530

    adding close method to store data in a fixed file_name

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ To scrape a particular category in scrapy pass in arguments like so:
 scrapy crawl books -a category="http://books.toscrape.com/catalogue/category/books/historical-fiction_4/index.html"
 
 (which is the link of the category that we wish to scrape)
+
+### Storing data in a csv file
+
+To store data in a csv type in:
+scrapy crawl books -a category="http://books.toscrape.com/catalogue/category/books/historical-fiction_4/index.html" -o items.csv
+
+But ultimately the file will be saved as foobar.csv since in books.py we have created a close function.
--- books_crawler/books_crawler/spiders/books.py
+++ books_crawler/books_crawler/spiders/books.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import os
+import glob
+
 # from scrapy.spiders import CrawlSpider, Rule
 from scrapy import Spider
 from scrapy.linkextractors import LinkExtractor
@@ -78,71 +81,6 @@ class BooksSpider(Spider):
 			"book_no_of_reviews": book_no_of_reviews
 		}
 
-
-
-# =========================================
-# CODE USING SELENIUM
-# =========================================
-
-# class BooksSpider(Spider):
-#     name = 'books'
-#     allowed_domains = ['books.toscrape.com']
-
-#     def start_requests(self):
-#     	# import ipdb; ipdb.set_trace()
-#     	# we won't get the response since start_urls is not defined.
-#     	self.driver = webdriver.Chrome('/home/sheron/Downloads/chromedriver')
-#     	self.driver.get('http://books.toscrape.com')
-
-#     	# Creating a selector
-#     	sel = Selector(text=self.driver.page_source)
-#     	book_urls = sel.xpath('//h3/a/@href').extract()
-
-#     	for book_url in book_urls:
-#     		url = 'http://books.toscrape.com/' + book_url
-#     		yield Request(url, callback=self.parse_book)
-
-
-#     	while True:
-#     		try:
-#     			next_page = self.driver.find_element_by_xpath('//a[text()="next"]')
-#     			sleep(3)
-#     			self.logger.info('Sleeping for 3 seconds')
-#     			next_page.click()
-
-#     			sel = Selector(text=self.driver.page_source)
-#     			book_urls = sel.xpath('//h3/a/@href').extract()
-
-#     			for book_url in book_urls:
-#     				url = 'http://books.toscrape.com/catalogue/' + book_url
-
-
-#     				yield Request(url, callback=self.parse_book)
-
-#     		except NoSuchElementException:
-#     			self.logger.info('No more pages to load.')
-#     			self.driver.quit()
-#     			break
-
-#     def parse_book(self, response):
-#     	pass
-
-
-
-# =======================================
-# Using CrawlSpider here.
-# =======================================
-
-# class BooksSpider(CrawlSpider):
-#     name = 'books'
-#     allowed_domains = ['books.toscrape.com']
-#     start_urls = ['http://books.toscrape.com/']
-
-#     # passing args
-#     # rules = (Rule(LinkExtractor(deny_domains=('google.com')), callback='parse_page', follow=True),)
-#     rules = (Rule(LinkExtractor(allow=('music')), callback='parse_page', follow=True),)
-
-#     # rules = (Rule(LinkExtractor(), callback='parse_page', follow=True),)
-
-#     def parse_page(self, response):
-#         pass
+	def close(self, reason):
+		csv_file = max(glob.iglob('*.csv'), key=os.path.getctime)
+		os.rename(csv_file, 'foobar.csv')

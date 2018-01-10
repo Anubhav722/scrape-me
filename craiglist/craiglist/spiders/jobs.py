093f6adb62ca8b3691c2b6ab7c4bd38e0b55205a
Anubhav722 <anubhavs286@gmail.com>
2018-01-11 03:03:10 +0530

    add scrapy code for first page

--- craiglist/craiglist/spiders/jobs.py
+++ craiglist/craiglist/spiders/jobs.py
@@ -7,9 +7,28 @@ class JobsSpider(scrapy.Spider):
     allowed_domains = ['newyork.craigslist.org']
     start_urls = ['https://newyork.craigslist.org/search/egr']
 
-    def parse(self, response):
-        listings = response.xpath('//a[@class="result-title hdrlnk"]/text()').extract()
 
-        for listing in listings:
-        	# print(listing)
-        	yield {'Listing': listing}
+    def parse(self, response):
+    	listings = response.xpath('//li[@class="result-row"]')
+
+    	for listing in listings:
+   			date = listing.xpath('.//*[@class="result-date"]/@datetime').extract_first()
+   			link = listing.xpath('.//a[@class="result-title hdrlnk"]/@href').extract_first()
+   			text = listing.xpath('.//a[@class="result-title hdrlnk"]/text()').extract_first()
+
+   			yield {
+   				'date': date,
+   				'link': link,
+   				'text': text
+   			}
+
+
+    # Commented out since it's good to fetch from outer class. and also we want
+    # to fetch more details other than just text
+
+    # def parse(self, response):
+    #     listings = response.xpath('//a[@class="result-title hdrlnk"]/text()').extract()
+
+    #     for listing in listings:
+    #     	# print(listing)
+    #     	yield {'Listing': listing}

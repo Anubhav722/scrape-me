d918b2af3e89284ef72f32b2267e07f22209fd6c
Anubhav722 <anubhavs286@gmail.com>
2018-01-13 21:47:23 +0530

    add job desc for craiglist

--- craiglist/craiglist/spiders/jobs.py
+++ craiglist/craiglist/spiders/jobs.py
@@ -16,14 +16,46 @@ class JobsSpider(scrapy.Spider):
             link = listing.xpath('.//a[@class="result-title hdrlnk"]/@href').extract_first()
             text = listing.xpath('.//a[@class="result-title hdrlnk"]/text()').extract_first()
 
-            yield {
-                'date': date,
-                'link': link,
-                'text': text}
+            # yield {
+            #     'date': date,
+            #     'link': link,
+            #     'text': text}
+
+            # to transfer the data from parse method to parse_listing use `meta`.
+            yield scrapy.Request(link, callback=self.parse_listing,
+                                meta={'date':date,
+                                      'link': link,
+                                      'date': date})
 
         next_page_url = response.xpath('//*[@class="button next"]/@href').extract_first()
         if next_page_url:
             yield scrapy.Request(response.urljoin(next_page_url), callback=self.parse)
+
+    def parse_listing(self, response):
+
+        # fetching the values from meta passed from above method
+        date = response.meta.get('date')
+        link = response.meta.get('link')
+        text = response.meta.get('text')
+
+        compensation = response.xpath('//*[@class="attrgroup"]/span[1]/b/text()').extract_first()
+        employment_type = response.xpath('//*[@class="attrgroup"]/span[2]/b/text()').extract_first()
+
+        job_profile = response.xpath('//*[@id="titletextonly"]/text()').extract_first()
+
+        job_description = response.xpath('//*[@id="postingbody"]/text()').extract()
+        job_description = " ".join(job_description)
+
+        yield {'date': date,
+               'link': link,
+               'text': text,
+               'compensation': compensation,
+               'employment_type': employment_type,
+               'job_profile': job_profile,
+               'job_description': job_description}
+
+
+
 
 
     # Commented out since it's good to fetch from outer class. and also we want
